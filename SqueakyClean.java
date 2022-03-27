70f0d1cb75ff78646dc34f57c07f88d271492b55
Jennifer <jzhao532@gmail.com>
2022-03-27 16:10:12 +0200

    LogLevels y SqueakyClean a medias

---
+++ SqueakyClean.java
@@ -0,0 +1,38 @@
+public class SqueakyClean {
+    static boolean isGreekChar(char c){
+        return c == 'α'|| c == 'β'|| c == 'γ'|| c == 'δ'|| c == 'ε'|| c == 'ζ'|| c == 'η'|| c == 'θ'|| c == 'ι'|| c == 'κ'|| c == 'λ'|| c == 'μ'|| c == 'ν'|| c == 'ξ'|| c == 'ο'|| c == 'π'|| c == 'ρ'|| c == 'σ'
+                || c == 'τ'|| c == 'υ'|| c == 'φ'|| c == 'χ'|| c == 'ψ'|| c == 'ω';
+    }
+
+    public static String clean(String identifier){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < identifier.length(); i++){
+            char current = identifier.charAt(i);
+            if(Character.isWhitespace(current)){
+                stringBuilder.append('_');
+            }else if(Character.isISOControl(current)){
+                stringBuilder.append("CTRL");
+            }else if(current == '-'){
+                i++;
+                current = Character.toUpperCase(identifier.charAt(i));
+                stringBuilder.append(current);
+            }else if(isGreekChar(current)){
+                continue;
+            }else if(current == '\\'){
+                continue;
+            }
+            else{
+                stringBuilder.append(current);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println(clean("my   Id"));
+        System.out.println(clean("my\0Id"));
+
+        System.out.println(clean("\uD83D\uDE00\uD83D\uDE00\uD83D\uDE00"));
+    }
+}
+
